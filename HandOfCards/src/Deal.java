89423d604c91b4e32c87725b82311ebf24e1a395
FanlingGU <61821720+FanlingGU@users.noreply.github.com>
2020-03-22 17:24:33 +0800

    a little change

--- HandOfCards/src/Deal.java
+++ HandOfCards/src/Deal.java
@@ -9,10 +9,10 @@ public class Deal {
         String[] card = new String[5];
         System.out.println("Please Enter Your 5 cards");
         for(int i=0; i <card.length;i++){
-            System.out.println("ScannerTest, Please Enter Your "+(i+1)+" card, and end with enter");
+            System.out.println("Please Enter Your "+(i+1)+" card, and end with enter");
            card[i] = enterCards.nextLine();
-           if(card[i].isEmpty()||card[i].isBlank()){
-               System.out.println("You need to entre your card, it can't be empty.");
+           while(card[i].isEmpty()||card[i].isBlank()){
+               System.out.println("You need to entre your "+(i+1)+" card, it can't be empty.");
                card[i] = enterCards.nextLine();
            }
         }
@@ -20,7 +20,8 @@ public class Deal {
         System.out.println(" your cards are :" + Arrays.toString(hand.cards));
 
     }
-    
+
+
     public static void main(String[] args){
         Deal deal= new Deal();
         deal.Scanner();
